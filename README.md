45f030fd4186f4f335e519f5449cdbcd4f14a90b
n.trofimov <n.trofimov@MacBook-Pro-hhuser.local>
2025-01-02 18:23:39 +0300

    refactore to lazyvim

---
+++ README.md
@@ -0,0 +1,4 @@
+# 💤 Personal nvim config based on LazyVim
+
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+      },
+    },
+  },
+}
