2b2a5e9ec4e9ddb500672506c30c7708e16984f4
n.trofimov <n.trofimov@MacBook-Pro-hhuser.local>
2025-01-02 18:59:31 +0300

    fix relative line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local o = vim.opt -- Create a shorthand for vim.opt
+
+o.number = true -- Enable absolute line numbers
+o.relativenumber = false -- Enable relative line numbers
